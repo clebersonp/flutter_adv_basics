bb470b9ed6a4676569da8faaf30b7da471495a66
Cleberson <pauluci.cleberson@gmail.com>
2024-01-27 18:16:48 -0300

    Refactor main dart file, replace all material widget code to the Quiz widget class

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_adv_basics/screen/home/start_screen.dart';
+import 'package:flutter_adv_basics/stateful/quiz.dart';
 
-void main() => runApp(const QuizApp());
-
-class QuizApp extends StatelessWidget {
-  const QuizApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.indigo, Colors.blueAccent],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    );
-  }
-}
+void main() => runApp(const Quiz());
