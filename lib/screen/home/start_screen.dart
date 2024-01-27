d0c11c6988e66866f090ff220da5fcadd8a56bd7
Cleberson <pauluci.cleberson@gmail.com>
2024-01-27 17:29:17 -0300

    Create a class startScreen in another dart file

---
+++ lib/screen/home/start_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset('assets/images/quiz-logo.png', width: 300),
+          const SizedBox(height: 80),
+          const Text(
+            "Learn Flutter the fun way!",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24.0,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              side: const BorderSide(color: Colors.white60),
+              foregroundColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+              textStyle: const TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            icon: const Icon(Icons.arrow_forward, size: 20.0),
+            label: const Text("Start Quiz"),
+          ),
+        ],
+      ),
+    );
+  }
+}
