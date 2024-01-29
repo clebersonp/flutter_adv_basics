55ae41dc178b7e813b0fa6a58d36bba345132ec6
Cleberson <pauluci.cleberson@gmail.com>
2024-01-29 10:30:42 -0300

    Created custom button for answers

---
+++ lib/buttons/answer_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton({
+    super.key,
+    required this.onTap,
+    required this.answerText,
+  });
+
+  final void Function() onTap;
+  final String answerText;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(40.0),
+          ),
+          backgroundColor: const Color.fromARGB(255, 20, 6, 105),
+          foregroundColor: Colors.white,
+          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10)),
+      child: Text(answerText),
+    );
+  }
+}
