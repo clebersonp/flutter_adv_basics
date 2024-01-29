3d2cb325ea572527681bc039c9e8aa4b00c560db
Cleberson <pauluci.cleberson@gmail.com>
2024-01-29 11:55:37 -0300

    Added a method to shuffle the answers before use it

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -6,4 +6,11 @@ class QuizQuestion {
   // properties
   final String text;
   final List<String> answers;
+
+  List<String> getShuffledAnswers() {
+    // make a copy of answers list to the memory
+    final List<String> shuffledAnswers = List.of(answers);
+    shuffledAnswers.shuffle();
+    return shuffledAnswers;
+  }
 }
