a60e25ce3fc485ba6e905731815bb9160f64ba17
Cleberson <pauluci.cleberson@gmail.com>
2024-01-29 09:37:54 -0300

    Wrapped the Column widget with SizedBox widget, it's another approach to configure columns

--- lib/screen/questions/questions_screen.dart
+++ lib/screen/questions/questions_screen.dart
@@ -10,6 +10,19 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Questions Screen');
+    // sizedBox is another solution instead of using the Center widget
+    return SizedBox(
+      width: double.infinity, // special value to use as much space as possible
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('The question...'),
+          const SizedBox(height: 30),
+          ElevatedButton(onPressed: () {}, child: const Text('Answer 1')),
+          ElevatedButton(onPressed: () {}, child: const Text('Answer 2')),
+          ElevatedButton(onPressed: () {}, child: const Text('Answer 3')),
+        ],
+      ),
+    );
   }
 }
