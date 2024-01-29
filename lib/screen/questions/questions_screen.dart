56a7f688449a78cf7168b51b2dc2a3eed7a79cf0
Cleberson <pauluci.cleberson@gmail.com>
2024-01-29 10:51:48 -0300

    Import dummy questions and answers and put them into the code

--- lib/screen/questions/questions_screen.dart
+++ lib/screen/questions/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_adv_basics/buttons/answer_button.dart';
+import 'package:flutter_adv_basics/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -11,20 +12,27 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
+    // using the dummy questions list
+    final currentQuestion = questions[0];
+
     // sizedBox is another solution instead of using the Center widget
     return SizedBox(
       width: double.infinity, // special value to use as much space as possible
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'The question...',
-            style: TextStyle(color: Colors.white),
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+            ),
           ),
           const SizedBox(height: 30),
-          AnswerButton(onTap: () {}, answerText: 'Answer 1'),
-          AnswerButton(onTap: () {}, answerText: 'Answer 2'),
-          AnswerButton(onTap: () {}, answerText: 'Answer 3'),
+          AnswerButton(onTap: () {}, answerText: currentQuestion.answers[0]),
+          AnswerButton(onTap: () {}, answerText: currentQuestion.answers[1]),
+          AnswerButton(onTap: () {}, answerText: currentQuestion.answers[2]),
+          AnswerButton(onTap: () {}, answerText: currentQuestion.answers[3]),
         ],
       ),
     );
