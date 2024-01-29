3860568b6d4dbd498f1ad50443404b2a4bd527eb
Cleberson <pauluci.cleberson@gmail.com>
2024-01-29 09:13:41 -0300

    Created a new class to store quiz data for questions and answers

---
+++ lib/models/quiz_question.dart
@@ -0,0 +1,9 @@
+// class to store data about quiz questions and answers
+class QuizQuestion {
+  // constructor
+  const QuizQuestion(this.text, this.answers);
+
+  // properties
+  final String text;
+  final List<String> answers;
+}
